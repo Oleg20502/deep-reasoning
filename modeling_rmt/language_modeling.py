f5a29f02edc7caf33ef26c7f628deee8d69036cf
booydar <ayd98@mail.ru>
2025-03-26 14:44:12 +0300

    flake8

--- modeling_rmt/language_modeling.py
+++ modeling_rmt/language_modeling.py
@@ -3,6 +3,7 @@ import torch
 from torch.nn import CrossEntropyLoss
 from transformers.modeling_outputs import CausalLMOutputWithCrossAttentions
 
+
 class MemoryCell(torch.nn.Module):
     def __init__(self, base_model, num_mem_tokens):
         super().__init__()
@@ -12,7 +13,7 @@ class MemoryCell(torch.nn.Module):
     def create_memory(self, num_mem_tokens):
         self.num_mem_tokens = num_mem_tokens
         embeddings = self.model.get_input_embeddings()
-        memory_dim =  getattr(self.model.config, 'n_embd', self.model.config.hidden_size)
+        memory_dim = getattr(self.model.config, 'n_embd', self.model.config.hidden_size)
         memory_weights = torch.randn((num_mem_tokens, memory_dim)) * embeddings.weight.data.std()
         self.register_parameter('memory', torch.nn.Parameter(memory_weights, requires_grad=True))
 
@@ -32,13 +33,14 @@ class MemoryCell(torch.nn.Module):
         out, new_memory_state = self.process_output(out, **kwargs)
 
         return out, new_memory_state
-    
+
     def generate(self, input_ids, memory_state, attention_mask, **generate_kwargs):
         if memory_state is None:
             memory_state = self.set_memory(input_ids.shape)
 
         seg_kwargs = self.process_input(input_ids, memory_state, attention_mask=attention_mask)
-        out = self.model.generate(inputs_embeds=seg_kwargs['inputs_embeds'], attention_mask=seg_kwargs['attention_mask'], **generate_kwargs)
+        out = self.model.generate(inputs_embeds=seg_kwargs['inputs_embeds'],
+                                  attention_mask=seg_kwargs['attention_mask'], **generate_kwargs)
         return out
 
     def process_input(self, input_ids, memory_state, **kwargs):
@@ -55,7 +57,7 @@ class MemoryCell(torch.nn.Module):
             seg_kwargs['attention_mask'] = self.pad_attention_mask(kwargs['attention_mask'], inputs_embeds.shape)
         seg_kwargs['output_hidden_states'] = True
         return seg_kwargs
-    
+
     def pad_attention_mask(self, attention_mask, shape):
         if self.num_mem_tokens in {0, None}:
             return attention_mask
@@ -63,32 +65,33 @@ class MemoryCell(torch.nn.Module):
             mask = torch.ones(*shape[:2], dtype=torch.int64).to(attention_mask.device)
             mask[:, self.num_mem_tokens:-self.num_mem_tokens] = attention_mask
             return mask
-    
+
     def process_output(self, model_outputs, **kwargs):
         if self.num_mem_tokens not in {0, None}:
             out = CausalLMOutputWithCrossAttentions()
             memory_state = model_outputs.hidden_states[-1][:, -self.num_mem_tokens:]
             out['logits'] = model_outputs.logits[:, self.num_mem_tokens:-self.num_mem_tokens]
-            
+
             if kwargs.get('output_hidden_states'):
-                out['hidden_states'] = [lh[:, self.num_mem_tokens:-self.num_mem_tokens] for lh in model_outputs.hidden_states]
+                out['hidden_states'] = [lh[:, self.num_mem_tokens:-self.num_mem_tokens]
+                                        for lh in model_outputs.hidden_states]
             if kwargs.get('output_attentions'):
                 out['attentions'] = model_outputs['attentions']
         else:
             memory_state = None
             out = model_outputs
-            
-        return out, memory_state 
+
+        return out, memory_state
 
 
-import random
 class RecurrentWrapper(torch.nn.Module):
     def __init__(self, memory_cell, **rmt_kwargs):
         super().__init__()
         self.memory_cell = memory_cell
         self.rmt_config = rmt_kwargs
 
-    def forward(self, input_ids, labels=None, labels_mask=None, inputs_embeds=None, attention_mask=None, output_attentions=None, output_hidden_states=None):
+    def forward(self, input_ids, labels=None, labels_mask=None, inputs_embeds=None, attention_mask=None,
+                output_attentions=None, output_hidden_states=None):
         memory_state = None
         segmented = self.segment(input_ids=input_ids, inputs_embeds=inputs_embeds, attention_mask=attention_mask)
 
@@ -98,12 +101,12 @@ class RecurrentWrapper(torch.nn.Module):
             cell_outputs.append(cell_out)
             self.manage_gradients(memory_state, seg_num)
 
-        out = self.process_outputs(cell_outputs, labels=labels, 
+        out = self.process_outputs(cell_outputs, labels=labels,
                                    labels_mask=labels_mask,
-                                   output_attentions=output_attentions, 
+                                   output_attentions=output_attentions,
                                    output_hidden_states=output_hidden_states)
         return out
-    
+
     def generate(self, input_ids, attention_mask, **generate_kwargs):
         memory_state = None
         segmented = self.segment(input_ids=input_ids, attention_mask=attention_mask)
@@ -128,7 +131,7 @@ class RecurrentWrapper(torch.nn.Module):
                         segments.append({k: k_seg})
 
         return segments
-    
+
     def split_tensor(self, tensor):
         align = self.rmt_config.get('segment_alignment')
         segment_size = self.rmt_config.get('segment_size')
@@ -148,7 +151,8 @@ class RecurrentWrapper(torch.nn.Module):
     def process_outputs(self, cell_outputs, **kwargs):
         out = CausalLMOutputWithCrossAttentions()
         full_logits = torch.cat([o.logits for o in cell_outputs], dim=1)
-        full_hidden_states = tuple([torch.cat(layer_hs, dim=1) for layer_hs in zip(*[o.hidden_states for o in cell_outputs])])
+        full_hidden_states = tuple([torch.cat(layer_hs, dim=1)
+                                    for layer_hs in zip(*[o.hidden_states for o in cell_outputs])])
 
         labels = kwargs.get('labels')
         if labels is not None:
@@ -156,7 +160,7 @@ class RecurrentWrapper(torch.nn.Module):
             shift_logits = full_logits[..., :-1, :].contiguous()
             flat_labels = shift_labels.view(-1)
             flat_logits = shift_logits.view(-1, shift_logits.size(-1))
-            
+
             loss_fct = CrossEntropyLoss()
             labels_mask = kwargs.get('labels_mask')
             if labels_mask is not None:
@@ -164,7 +168,7 @@ class RecurrentWrapper(torch.nn.Module):
 
                 flat_labels = flat_labels[shift_mask.view(-1)]
                 flat_logits = flat_logits[shift_mask.view(-1)]
-                
+
             out['loss'] = loss_fct(flat_logits, flat_labels)
         else:
             out['loss'] = 0
@@ -182,14 +186,12 @@ class RecurrentWrapper(torch.nn.Module):
                 if any([sk in key for sk in segment_keys]):
                     out[f'{key}_{seg_num}'] = value
 
-        return out 
-        
+        return out
+
     def manage_gradients(self, memory_state, seg_num):
         k2, max_n_segments = self.rmt_config.get('k2'), self.rmt_config.get('max_n_segments')
-        if seg_num == 0 \
-            or k2 in {-1, None} \
-            or seg_num + k2 > max_n_segments:
-                return True
-        
+        if seg_num == 0 or k2 in {-1, None} or seg_num + k2 > max_n_segments:
+            return True
+
         memory_state = memory_state.detach()
         return False
